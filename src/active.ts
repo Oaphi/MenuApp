f254e58707a95bd837388ddf4b5e61e5868c51fa
Oleg Valter <o.a.philippov@gmail.com>
2021-07-13 16:36:26 +0300

    moved UI getters from module, fixed types

---
+++ src/active.ts
@@ -0,0 +1,39 @@
+type ErrLoggable<T> = T & { onError?: (err: string | Error) => void };
+
+type ActiveDocOptions = ErrLoggable<{
+    type?: DocTypes;
+}>;
+
+type UserInterfaceGetter = () => GoogleAppsScript.Base.Ui;
+
+const isCorrectUIgetter_ = (getter: UserInterfaceGetter) => {
+    try {
+        return !!getter();
+    } catch (error) {
+        return false;
+    }
+};
+
+const getActiveUI_ = ({
+    type = DocTypes.SPREADSHEET,
+    onError = console.warn,
+}: ActiveDocOptions = {}) => {
+    const ActiveDocMap: Map<DocTypes, UserInterfaceGetter> = new Map();
+
+    ActiveDocMap.set(DocTypes.DOCUMENT, DocumentApp.getUi);
+    ActiveDocMap.set(DocTypes.FORM, FormApp.getUi);
+    ActiveDocMap.set(DocTypes.SPREADSHEET, SpreadsheetApp.getUi);
+    ActiveDocMap.set(DocTypes.PRESENTATION, SlidesApp.getUi);
+
+    const getter = ActiveDocMap.get(type)!;
+
+    try {
+        const valid = isCorrectUIgetter_(getter)
+            ? getter
+            : [...ActiveDocMap.values()].find(isCorrectUIgetter_)!;
+        return valid();
+    } catch (error) {
+        onError(error);
+        return null;
+    }
+};
